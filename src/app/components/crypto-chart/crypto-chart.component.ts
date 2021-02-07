3bac3a3b03e8499046e8be4453e90ec79365ae59
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2021-02-07 15:32:45 +0000

    buttons added and styled, chart styled, hostListener created to make chart more mobile friendly

--- src/app/components/crypto-chart/crypto-chart.component.ts
+++ src/app/components/crypto-chart/crypto-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Type, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Type, ViewChild, HostListener } from '@angular/core';
 import { ApidataService } from '../../services/apidata.service';
 import { HistoricPrice } from '../../models/historic.price'
 import { Source } from '../../models/source';
@@ -30,145 +30,145 @@ export class CryptoChartComponent implements OnInit {
   private svg: any;
   private line: any;
 
-  constructor(private Apidataservice: ApidataService,
+  constructor(private ApidataService: ApidataService,
     private container: ElementRef) {  
       this.width = 960 - this.margin.left - this.margin.right;
       this.height = 500 - this.margin.top - this.margin.bottom;
     }
 
-  ngOnInit(): void {
-    this.fetchBitcoinData();
-  }
-
-  createChart() {
-    this.buildSvg();
-    this.addXandYAxis();
-    this.drawLineAndPath();
-  }
-
-  fetchBitcoinData() {
-    this.Apidataservice.get(this.BITCOIN)
-      .subscribe(res => {
-        this.historicBitcoinData = res.Data.Data;
-        this.fetchEthereumData();
-      }, err => {
-        console.error(err);
-      })
-  }
-
-  fetchEthereumData() {
-    this.Apidataservice.get(this.ETHEREUM)
-      .subscribe(res => {
-        this.historicEthereumData = res.Data.Data;
-        this.mergedData = [...this.historicBitcoinData, ...this.historicEthereumData];
-        this.createChart();
-      }, err => {
-        console.error(err);
-      })
-  }
-
-  changeChart(type: string) {
-    this.selectedType = type;
-    this.redrawChart();
-  }
-
-  redrawChart() {
-    d3.selectAll("svg > *").remove();
-    this.createChart();
-  }
-
-  private buildSvg() {
-    this.svg = d3.select('svg')
-      .append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-  }
-
-  private addXandYAxis() {
-    this.width = parseInt(d3.select("#chart").style("width")) - this.margin.left - this.margin.right - 40
-    this.height = parseInt(d3.select("#chart").style("height")) - this.margin.top - this.margin.bottom 
-    this.x = d3Scale.scaleTime().range([0, this.width]);
-    this.y = d3Scale.scaleLinear().range([this.height, 0])
-    this.x.domain(d3Array.extent(this.mergedData, (d) => new Date(d.time * 1000)));
-    let data: HistoricPrice[] = [];
-    if (this.selectedType === 'both') {
-      data = this.mergedData;
-    } else if (this.selectedType === 'ethereum') {
-      data = this.historicEthereumData
-    } else if (this.selectedType === 'bitcoin') {
-      data = this.historicBitcoinData
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+      event.target.innerWidth;
+      d3.selectAll("svg > *").remove();
+      this.createChart();
     }
-    this.y.domain(d3Array.extent(data, (d) => d.close));
-    this.svg.append('g')
-      .attr('transform', 'translate(0,' + this.height + ')')
-      .call(d3Axis.axisBottom(this.x));
-    this.svg.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3Axis.axisLeft(this.y));
-    this.svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + this.height + ")")
-      .call(this.x)
-      .selectAll("text")
-      .attr("y", 0)
-      .attr("x", 9)
-      .attr("dy", ".35em")
-      .attr("transform", "rotate(90)")
-      .style("text-anchor", "start")
-    this.svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - this.margin.left - 20)
-      .attr("x",0 - (this.height / 2))
-      .attr("dy", "1em")
-      .text("USD")
-      .style("fill", "white")
-  }
+  
+  
+    ngOnInit() {
+      this.fetchBitcoinData();
+    }
+  
+    changeChart(type: string) {
+      this.selectedType = type;
+      this.redrawChart();
+    }
+  
+    createChart() {
+      this.buildSvg();
+      this.addXandYAxis();
+      this.drawLineAndPath();
+    }
+  
+  
+    redrawChart() {
+      d3.selectAll("svg > *").remove();
+      this.createChart();
+    }
+  
+    fetchBitcoinData() {
+      this.ApidataService.get(this.BITCOIN)
+        .subscribe(res => {
+          this.historicBitcoinData = res.Data.Data;
+          this.fetchEthereumData();
+        }, err => {
+          console.error(err);
+        })
+    }
 
-  private drawBitcoinLineGraph() {
-    this.svg.append('path')
-      .datum(this.historicBitcoinData)
-      .attr('class', 'line')
-      .attr("fill", "none")
-      .attr('d', this.line)
-      .attr("stroke", 'orange')
-      .attr("stroke-width", 2)
+    fetchEthereumData() {
+      this.ApidataService.get(this.ETHEREUM)
+        .subscribe(res => {
+          this.historicEthereumData = res.Data.Data;
+          this.mergedData = [...this.historicBitcoinData, ...this.historicEthereumData];
+          this.createChart();
+        }, err => {
+          console.error(err);
+        })
+    }
+  
+    private buildSvg() {
+      this.svg = d3.select('svg')
+        .append('g')
+        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+    }
 
-    this.svg.append("text")
-      .attr("transform", "translate(" + (this.width + 2) + "," + this.y(this.historicBitcoinData[this.historicBitcoinData.length - 1].close) + ")")
-      .attr("dy", ".35em")
-      .attr("text-anchor", "start")
-      .style("fill", "orange")
-      .text("BTC");
-  }
+    private addXandYAxis() {
+      this.width = parseInt(d3.select("#chart").style("width")) - this.margin.left - this.margin.right - 40
+      this.height = parseInt(d3.select("#chart").style("height")) - this.margin.top - this.margin.bottom 
+      this.x = d3Scale.scaleTime().range([0, this.width]);
+      this.y = d3Scale.scaleLinear().range([this.height, 0])
+      this.x.domain(d3Array.extent(this.mergedData, (d) => new Date(d.time * 1000)));
+      let data: HistoricPrice[] = [];
+      if (this.selectedType === 'both') {
+        data = this.mergedData;
+      } else if (this.selectedType === 'ethereum') {
+        data = this.historicEthereumData
+      } else if (this.selectedType === 'bitcoin') {
+        data = this.historicBitcoinData
+      }
+      this.y.domain(d3Array.extent(data, (d) => d.close));
+      this.svg.append('g')
+        .attr('transform', 'translate(0,' + this.height + ')')
+        .call(d3Axis.axisBottom(this.x));
+      this.svg.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3Axis.axisLeft(this.y));
+      this.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - this.margin.left - 20)
+        .attr("x",0 - (this.height / 2))
+        .attr("dy", "1em")
+        .text("USD")
+        .style("fill", "white")
+    }
+  
+  
+    private drawBitcoinLineGraph() {
+      this.svg.append('path')
+        .datum(this.historicBitcoinData)
+        .attr('class', 'line')
+        .attr("fill", "none")
+        .attr('d', this.line)
+        .attr("stroke", 'orange')
+        .attr("stroke-width", 2)
+
+      this.svg.append("text")
+        .attr("transform", "translate(" + (this.width + 2) + "," + this.y(this.historicBitcoinData[this.historicBitcoinData.length - 1].close) + ")")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "start")
+        .style("fill", "orange")
+        .text("BTC");
+    }
 
   private drawEthereumLineGraph() {
-    this.svg.append('path')
-      .datum(this.historicEthereumData)
-      .attr('class', 'line')
-      .attr("fill", "none")
-      .attr('d', this.line)
-      .attr("stroke", 'Blue')
-      .attr("stroke-width", 2);
-
-    this.svg.append("text")
-      .attr("transform", "translate(" + (this.width + 2) + "," + this.y(this.historicEthereumData[this.historicEthereumData.length - 1].close) + ")")
-      .attr("dy", ".35em")
-      .attr("text-anchor", "start")
-      .style("fill", "steelBlue")
-      .text("ETH");
-  }
-
-  private drawLineAndPath() {
-    this.line = d3Shape.line()
-      .x((d: any) => this.x(new Date(d.time * 1000)))
-      .y((d: any) => this.y(d.close));
-
-    if (this.selectedType === 'both') {
-      this.drawBitcoinLineGraph();
-      this.drawEthereumLineGraph();
-    } else if (this.selectedType === 'ethereum') {
-      this.drawEthereumLineGraph();
-    } else if (this.selectedType === 'bitcoin') {
-      this.drawBitcoinLineGraph();
+      this.svg.append('path')
+        .datum(this.historicEthereumData)
+        .attr('class', 'line')
+        .attr("fill", "none")
+        .attr('d', this.line)
+        .attr("stroke", 'Blue')
+        .attr("stroke-width", 2);
+  
+      this.svg.append("text")
+        .attr("transform", "translate(" + (this.width + 2) + "," + this.y(this.historicEthereumData[this.historicEthereumData.length - 1].close) + ")")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "start")
+        .style("fill", "steelBlue")
+        .text("ETH");
     }
-  }
+  
+    private drawLineAndPath() {
+      this.line = d3Shape.line()
+        .x((d: any) => this.x(new Date(d.time * 1000)))
+        .y((d: any) => this.y(d.close));
+  
+      if (this.selectedType === 'both') {
+        this.drawBitcoinLineGraph();
+        this.drawEthereumLineGraph();
+      } else if (this.selectedType === 'ethereum') {
+        this.drawEthereumLineGraph();
+      } else if (this.selectedType === 'bitcoin') {
+        this.drawBitcoinLineGraph();
+      }
+    }
 }
