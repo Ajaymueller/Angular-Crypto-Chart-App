75cf043fa39169d9da7cde4a792a543286558656
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2021-02-07 14:09:37 +0000

    routes added, components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { CryptoChartComponent } from './components/crypto-chart/crypto-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CryptoChartComponent}, 
+  { path: 'about', component: AboutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
